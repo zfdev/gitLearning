c0c44c49741a51427e65dd205dc541440924bb8f
Jason Zhang <v-zhlong@microsoft.com>
2015-08-26 15:13:08 +0800

    Add note

--- jasonUI.js
+++ jasonUI.js
@@ -1,5 +1,6 @@
 window.JZ = widnow.JZ || {};
-(function (win,$) {
+(function (win, $) {
+    //@param msg {String} debug information
     $.log = function (msg) {
         if(window.console){
             console.log(msg);
